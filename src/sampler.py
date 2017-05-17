3a16aa6a81b52dd404a0f11e3a4321b2fed14347
Jan Ladleif <Jan Ladleif>
2017-05-17 13:41:54 +0200

    improve article lookup and file handling

--- src/sampler.py
+++ src/sampler.py
@@ -1,8 +1,17 @@
 import sys
 import psycopg2
 import time
+import os
+import errno
 
 import tree
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
 # Connect to the database.
 try:
@@ -20,7 +29,7 @@ cur.execute(
     FROM comments
     """
 )
-articles = [tree.HateArticle(row[0]) for row in cur]
+articles = dict([(row[0], tree.HateArticle(row[0])) for row in cur])
 
 # Get the comments, without some of the clutter like text.
 cur = conn.cursor()
@@ -38,21 +47,22 @@ print("Number of comments before tree structuring: ", cur.rowcount)
 # Transform the comments into a tree structure. Dangling comments (which are answers to another
 # comment which is not in the data set) are discarded.
 for row in cur:
-    comment = tree.Comment(row[0], row[1], row[2], row[3])
-    for article in articles:
-        if article.addComment(comment):
-            break
+    article = articles.get(row[3])
+    if article != None:
+        comment = tree.Comment(row[0], row[1], row[2], row[3])
+        article.addComment(comment)
 
-commentCount = sum([article.getCommentCount() for article in articles])
+commentCount = sum([article.getCommentCount() for url, article in articles.items()])
 print("Number of comments after tree structuring:  ", commentCount)
 
 # Partition the data into three sets (20%, 20%, 60%)
-stats = [0, 0, 0]
-files = [open('output/part1.csv', 'w'), open('output/part2.csv', 'w'), open('output/part3.csv', 'w')]
+make_sure_path_exists('../output')
+stats = [1, 1, 1]
+files = [open('../output/part1.csv', 'w+'), open('../output/part2.csv', 'w+'), open('../output/part3.csv', 'w+')]
 targets = [.2, .2, .6]
 cursor = conn.cursor()
 start = time.time()
-for article in articles:
+for url, article in articles.items():
     article.partition(cursor, targets, stats, files)
 
 print("Target partitioning was ", [int(commentCount * target) for target in targets])
